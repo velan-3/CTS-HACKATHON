c737c97a0ab294f2f13cac88e6d2298eb8358e07
VaithiArul <vaitheeshvarsatyaprakash@gmail.com>
2024-09-05 16:53:07 +0530

    consultation frontend update

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,37 +1,37 @@
 async function fetchChartsConfig() {
-  const response = await fetch('/charts-config');
-  return await response.json();
+    const response = await fetch('/charts-config');
+    return await response.json();
 }
 
 async function renderCharts() {
-  const chartsConfig = await fetchChartsConfig();
-  
-  chartsConfig.forEach(config => {
-      const ctx = document.getElementById(config.id).getContext('2d');
-      new Chart(ctx, {
-          type: config.type,
-          data: {
-              labels: config.labels,
-              datasets: [{
-                  label: config.label,
-                  data: config.data,
-                  backgroundColor: config.bgColor,
-                  borderColor: config.borderColor || 'transparent',
-                  borderWidth: config.borderWidth || 1,
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      min: config.yMin,
-                      max: config.yMax,
-                      beginAtZero: true
-                  }
-              },
-              responsive: true
-          }
-      });
-  });
+    const chartsConfig = await fetchChartsConfig();
+
+    chartsConfig.forEach(config => {
+        const ctx = document.getElementById(config.id).getContext('2d');
+        new Chart(ctx, {
+            type: config.type,
+            data: {
+                labels: config.labels,
+                datasets: [{
+                    label: config.label,
+                    data: config.data,
+                    backgroundColor: config.bgColor,
+                    borderColor: config.borderColor || 'transparent',
+                    borderWidth: config.borderWidth || 1,
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        min: config.yMin,
+                        max: config.yMax,
+                        beginAtZero: true
+                    }
+                },
+                responsive: true
+            }
+        });
+    });
 }
 
 renderCharts();
